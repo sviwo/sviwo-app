f8a0762b6f3b7267c2d1e495e719f64dccdb55ad
Gao_wen <gavinegaowen@gmail.com>
2023-12-29 19:42:47 +0800

    fix frame

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,27 +2,86 @@ package cmd
 
 import (
 	"context"
+	"sviwo/internal/consts"
+	"sviwo/internal/controller"
+	"sviwo/internal/service"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
-
-	"github.com/gogf/template-single/internal/controller/hello"
 )
 
 var (
 	Main = gcmd.Command{
-		Name:  "main",
-		Usage: "main",
-		Brief: "start http server",
+		Name:  consts.ProjectName,
+		Usage: consts.ProjectUsage,
+		Brief: consts.ProjectBrief,
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
-			s.Group("/", func(group *ghttp.RouterGroup) {
-				group.Middleware(ghttp.MiddlewareHandlerResponse)
-				group.Bind(
-					hello.NewV1(),
+			// 启动管理后台gtoken
+			gfAdminToken, err := StartBackendGToken()
+			if err != nil {
+				return err
+			}
+			//管理后台路由组
+			s.Group("/backend", func(group *ghttp.RouterGroup) {
+				group.Middleware(
+					service.Middleware().CORS,
+					service.Middleware().Ctx,
+					service.Middleware().ResponseHandler,
 				)
+				//不需要登录的路由组绑定
+				group.Bind(
+					controller.Admin.Create, // 管理员
+					controller.Login,        // 登录
+				)
+				//需要登录的路由组绑定
+				group.Group("/", func(group *ghttp.RouterGroup) {
+					err := gfAdminToken.Middleware(ctx, group)
+					if err != nil {
+						panic(err)
+					}
+					group.Bind(
+						controller.Role,         // 角色
+						controller.Permission,   // 权限
+						controller.Admin.List,   // 管理员
+						controller.Admin.Update, // 管理员
+						controller.Admin.Delete, // 管理员
+						controller.Admin.Info,   // 查询当前管理员信息
+					)
+				})
 			})
+			//---------------------华丽的分割线-------------------
+			// 启动前台项目gtoken
+			frontendToken, err := StartFrontendGToken()
+			if err != nil {
+				return err
+			}
+			//前台项目路由组
+			s.Group("/frontend", func(group *ghttp.RouterGroup) {
+				group.Middleware(
+					service.Middleware().CORS,
+					service.Middleware().Ctx,
+					service.Middleware().ResponseHandler,
+				)
+				//不需要登录的路由组绑定
+				group.Bind(
+					controller.User.Register, //用户注册
+				)
+				//需要登录鉴权的路由组
+				group.Group("/", func(group *ghttp.RouterGroup) {
+					err := frontendToken.Middleware(ctx, group)
+					if err != nil {
+						return
+					}
+					//需要登录鉴权的接口放到这里
+					group.Bind(
+						controller.User.Info,           //当前登录用户的信息
+						controller.User.UpdatePassword, //当前用户修改密码
+					)
+				})
+			})
+			s.SetPort(8000) //设置端口
 			s.Run()
 			return nil
 		},
--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,1 +1,37 @@
 package consts
+
+const (
+	ProjectName              = "sviwo"
+	ProjectUsage             = "sviwo app server"
+	ProjectBrief             = "start http server"
+	Version                  = "v0.1.0"             // 当前服务版本(用于模板展示)
+	CaptchaDefaultName       = "CaptchaDefaultName" // 验证码默认存储空间名称
+	ContextKey               = "ContextKey"         // 上下文变量存储键名，前后端系统共享
+	FileMaxUploadCountMinute = 10                   // 同一用户1分钟之内最大上传数量
+	GTokenAdminPrefix        = "Admin:"             //gtoken登录 管理后台 前缀区分
+	GTokenFrontendPrefix     = "User:"              //gtoken登录 前台用户 前缀区分
+	//for admin
+	CtxAdminId      = "CtxAdminId"
+	CtxAdminName    = "CtxAdminName"
+	CtxAdminIsAdmin = "CtxAdminIsAdmin"
+	CtxAdminRoleIds = "CtxAdminRoleIds"
+	//for user
+	CtxUserId     = "CtxUserId"
+	CtxUserName   = "CtxUserName"
+	CtxUserAvatar = "CtxUserAvatar"
+	CtxUserSex    = "CtxUserSex"
+	CtxUserSign   = "CtxUserSign"
+	CtxUserStatus = "CtxUserStatus"
+	//for 登录相关
+	TokenType          = "Bearer"
+	CacheModeRedis     = 2
+	BackendServerName  = "Sviwo Backend"
+	MultiLogin         = true
+	FrontendMultiLogin = true
+	GTokenExpireIn     = 10 * 24 * 60 * 60
+	//统一管理错误提示
+	CodeMissingParameterMsg = "请检查是否缺少参数"
+	ErrLoginFaulMsg         = "登录失败，账号或密码错误"
+	ErrSecretAnswerMsg      = "密保问题不正确"
+	ResourcePermissionFail  = "没有权限操作"
+)
